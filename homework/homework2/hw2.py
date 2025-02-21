23372fff241470694e718e1989182c67cc4411bb
Charlotte Beckner <charlie_beckner@visitor-10-58-198-77.wifi.berkeley.edu>
2025-02-20 23:40:31 -0800

    Adding homework directory

---
+++ homework/homework2/hw2.py
@@ -0,0 +1,29 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+    # cd ~/Desktop
+
+# 2) What does ~/ mean?
+    # ~/ represents your home directory
+
+# 3) What's the difference between and abolute and a relative path in your terminal?
+    # an absolute path is a path that starts with ~/ to represent your home directory and lays out the entire path you want the command to follow. It can be used from any directory. A relative path is relative from your current directory. For example, you can type cd homework2 if you're in charlie_decal because relative to charlie_decal, homework2 is only one below it. 
+
+# 4) What command would bring you back to your home directory?
+    # cd 
+
+# 5) If you called rm ./ in your current working directory, what would it do? 
+    # remove your current working directory
+
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+    # git add adds your working directory into a staging area. Git commit then moves it from the staging area to your local repository. Git push pushes it from your local repository to a remote repository on the internet. 
+
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+    # git add will put your working directory into a staging area. Git status will then show a message that says "Changes to be submitted" and then show you an overview of the changes in your directory that have been put in your staging area and will be committed if you use git commit. 
+
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+    # I've found that I can sometimes get lost because I don't remember which directory I'm in or what my directory contains which can cause issues if I try to use a command on a directory that is not contained in my current one. I now like to use ls almost every time I enter a new directory so I can be clear of the path I've taken or the path I need to take. 
+
+# 9) How does cloning a repository differ from pulling from a repository?
+    # copying a repository copies the entire repository and all the files it contains onto your remote repository, whereas pulling a repository adds all of the changes people have made onto your repository. 
+
+# 10) Tell me a fun fact!
+    # Might be a little TMI but one of my toes is a little shorter than it should be because I broke it when I was little and it never properly healed. 
